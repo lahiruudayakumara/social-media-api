94c771911d3b9868fb2199d70898b0bb05a7cef8
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-01-09 12:00:00 +0530

    Add DB & auth middleware

---
+++ config/database.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log"
+	"os"
+	"social-media-api/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Auto migrate models
+	DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Comment{})
+}
---
+++ middleware/auth_middleware.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"net/http"
+	"social-media-api/utils"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Extract token from Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+			c.Abort()
+			return
+		}
+
+		// Split the header into "Bearer <token>"
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+		claims, err := utils.VerifyToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token: " + err.Error()})
+			c.Abort()
+			return
+		}
+
+		// Store user information in context for further use
+		c.Set("user_id", claims.UserID)
+		c.Set("email", claims.Email)
+
+		c.Next()
+	}
+}
